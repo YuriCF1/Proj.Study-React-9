48b3d4ecda700f4d26d0e02d9103ffeef63c906a
Yuri <yurifdev@gmail.com>
2023-01-18 18:50:13 -0300

    Creating context folder

---
+++ context-api/src/context/CounterContext.js
@@ -0,0 +1,19 @@
+// 1 - Criando o contexto
+
+import { createContext, useState } from "react";
+
+export const CounterContext = createContext();
+
+// 2 - Criando provider
+export const CounterContextProvider = ({ children }) => {
+  const [counter, setCounter] = useState(5);
+
+  return (
+    <CounterContext.Provider
+      value={{ counter, setCounter }}
+    >
+        {children}
+
+    </CounterContext.Provider>
+  );
+};
