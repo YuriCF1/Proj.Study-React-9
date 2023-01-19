378134ca2dd6fffbb5fc9c897bf6253f6d3b6e7a
Yuri <yurifdev@gmail.com>
2023-01-19 11:31:01 -0300

    Commenting

--- context-api/src/index.js
+++ context-api/src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* 2 - Criando Provider */}
-    <CounterContextProvider>
+    <CounterContextProvider> {/*Como ele tem o children, ele renderiza tudo que tem no App também*/}
       <App />
     </CounterContextProvider>
   </React.StrictMode>
