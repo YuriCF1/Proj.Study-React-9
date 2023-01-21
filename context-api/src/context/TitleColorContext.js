ed564c2ee0ba5296112cdcb480e3d345969cecfa
Yuri <yurifdev@gmail.com>
2023-01-20 23:52:59 -0300

    Comenting

--- context-api/src/context/TitleColorContext.js
+++ context-api/src/context/TitleColorContext.js
@@ -2,12 +2,12 @@ import { createContext, useReducer } from "react";
 
 export const TitleColorContext = createContext();
 
-export const titleColorReducer = (state, action) => {
+export const titleColorReducer = (state, action) => { //Funação para mudar o estado
   //switch
 };
 
 export const TitleColorContextProvider = ({ children }) => {
-    // State = Estado inicial definido "color: 'purple'" | dispatch = função que faz a mudança do valor | {color: "purple" = estado inicial}
+  // State = Estado inicial definido "color: 'purple'" | dispatch = função que faz a mudança do valor | {color: "purple" = estado inicial}
   const [state, dispatch] = useReducer(titleColorReducer, { color: "purple" });
 
   console.log("Title color context", state);
